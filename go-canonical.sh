0d856c0d21c66c103c9082325ea909ac46dac9c6
Manfred Touron <m@42.am>
2019-02-09 09:36:47 +0100

    chore: add google analytics

--- go-canonical.sh
+++ go-canonical.sh
@@ -12,14 +12,16 @@ while read line; do
     cat > docs/$package/index.html <<EOF
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta name="go-import" content="ultre.me/$package git $clone">
-        <meta name="go-source" content="ultre.me/$package $doc $doc/tree/master{/dir} $doc/tree/master{/dir}/{file}#L{line}">
-        <meta http-equiv="refresh" content="0; url=$homepage">
-    </head>
-    <body>
-        Nothing to see here. Please <a href="$homepage">move along</a>.
-    </body>
+  <head>
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-51632370-4"></script>
+    <script>window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date()); gtag('config', 'UA-51632370-4');</script>
+    <meta name="go-import" content="ultre.me/$package git $clone">
+    <meta name="go-source" content="ultre.me/$package $doc $doc/tree/master{/dir} $doc/tree/master{/dir}/{file}#L{line}">
+    <meta http-equiv="refresh" content="0; url=$homepage">
+  </head>
+  <body>
+    Nothing to see here. Please <a href="$homepage">move along</a>.
+  </body>
 </html>
 EOF
 done < go-canonical.csv
